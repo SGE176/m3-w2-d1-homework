e16a9e7a928a44b4716869177f11d450af7a7ea2
SGE176 <s.ge.176@westcliff.edu>
2022-03-14 19:47:26 -0500

    commit updated

--- app.js
+++ app.js
@@ -42,3 +42,78 @@ var stats =[
         'age': '35'
     }
 ]
+var MongoClient = require('mongodb').MongoClient;
+
+//statsdb is the database name
+var url = "mongodb://localhost:27017/statsdb";
+
+MongoClient.connect(url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, db) {
+    //    if (err) throw err;
+    //    console.log("Database-statsdb created!");
+    //    db.close();
+
+    var dbo = db.db("statsdb");
+    // dbo.createCollection("uscensus", function(err, res) {
+    //     if (err) throw err;
+    //     console.log("Collection-uscensus created!");
+    //     db.close();
+    // });
+
+    // dbo.collection("uscensus").insertMany(stats, function(err, res) {
+    //     if (err) throw err;
+    //         console.log("Number of documents inserted:" + res.insertedCount);
+    //         db.close();
+    // });  
+
+// var statsnew = [
+//         {
+//             'city': 'Pacoima', 
+//             'zip': '91331', 
+//             'state': 'CA', 
+//             'income': '60360',
+//             'age': '33'
+//         },
+//         {
+//             'city': 'Ketchikan', 
+//             'zip': '99950', 
+//             'state': 'AK', 
+//             'income': '000000',
+//             'age': '00'
+//         }
+//     ];
+//     dbo.collection("uscensus").insertMany(statsnew, function(err, res) {
+//         if (err) throw err;
+//             console.log("Number of documents inserted:" + res.insertedCount);
+//             db.close();
+//     });
+    // dbo.collection("uscensus").findOne({ city: "Corona", state: "NY"}, function(err, result){
+    //    if (err) throw err;
+    //          console.log("Zip of Corona, NY is " + result.zip);
+    //         db.close();
+    // });
+// var query = { state: /^CA/};
+//     dbo.collection("uscensus").find(query).toArray(function(err, result) {
+//             if (err) throw err;
+//                 console.log("Incomes of all cities in CA are " + result.map(item => (item["income"])));
+//                 db.close();
+//     });
+// var myquery = { state: "AK" };
+//     var newvalues = { $set: { income: "38910", age: "46" } };
+
+//     dbo
+//       .collection("uscensus")
+//       .updateOne(myquery, newvalues, function (err, result) {
+//         if (err) throw err;
+//         console.log("1 document updated - AK");
+//         db.close();
+//       });
+// var mysort = { state:1};
+//     dbo.collection("uscensus").find().sort(mysort).toArray(function(err, result) {
+//                 if (err) throw err;
+//                 console.log(`Sorted by state ascending list: `);
+//                 console.log(result);
+//                 db.close();
+//     });
+})
